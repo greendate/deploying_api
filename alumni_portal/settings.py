f05ad27afa82f08a573b5e5f9bdc0bfcea1b691c
Nikola Novarlic <35609096+greendate@users.noreply.github.com>
2023-03-11 13:18:49 +0300

    Update settings.py

--- alumni_portal/settings.py
+++ alumni_portal/settings.py
@@ -27,8 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
-    "*",
+    'http://localhost:3000',
 ]
 
 
